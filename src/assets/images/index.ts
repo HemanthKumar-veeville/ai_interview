c486da4c596044d883fa10d318457fe0b4632e24
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2025-01-22 12:05:31 +0530

    correct image format

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,7 @@
 import bg_01 from "./Tesco_UI_21Jan25_Ish-01.png";
 import bg_02 from "./Tesco_UI_21Jan25_Ish-02.png";
 import bg_03 from "./Tesco_UI_21Jan25_Ish-03.png";
-import tara from "./tara.png";
+import tara from "./tara.jpg";
 export {
   bg_01,
   bg_02,
